dab5d27b209b7e9e0a8ad475885f3a9cc2451860
copperkraft <copperkraft@gmail.com>
2017-05-23 09:10:43 +0300

    offers image finished

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
  */
 var gulp = require('gulp');
 var less = require('gulp-less');
+var concat = require('gulp-concat');
 
 gulp.task('hello', function() {
     console.log('Hello Zell');
@@ -11,5 +12,6 @@ gulp.task('hello', function() {
 gulp.task('less', function(){
     return gulp.src('less/imports.less')
         .pipe(less()) // Converts LESS to CSS with gulp-sass
-        .pipe(gulp.dest('css/style.css'))
+        .pipe(concat('styles.css'))
+        .pipe(gulp.dest('./'))
 });
